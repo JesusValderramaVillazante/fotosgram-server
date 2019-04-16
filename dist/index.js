ecf25bb493dbca362ad986c80d9fb8b323a8ebda
JesusValderramaVillazante <jesusvalderramavillazante@gmail.com>
2019-04-16 23:12:26 +0200

    levantar la base de datos mongodb y conectar nuestra aplicacion

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = require("./classes/server");
+const mongoose_1 = __importDefault(require("mongoose"));
 const usuario_1 = __importDefault(require("./routes/usuario"));
 const server = new server_1.Server();
 server.app.use('/user', usuario_1.default);
+mongoose_1.default.connect('mongodb://localhost:27017/fotosgram', { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+    if (err)
+        throw err;
+    console.log('base de datos online');
+});
 server.start(() => {
     console.log(`Servidor corriendo en el puerto ${server.port}`);
 });
--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
 import { Server } from './classes/server';
+import mongoose from 'mongoose';
 import userRoutes from './routes/usuario';
+
 const server = new Server();
 
 server.app.use('/user', userRoutes);
+
+mongoose.connect('mongodb://localhost:27017/fotosgram', {useNewUrlParser: true, useCreateIndex: true}, (err) => {
+    if(err) throw err;
+    console.log('base de datos online');
+});
 
 server.start(()=>{
     console.log(`Servidor corriendo en el puerto ${server.port}`)
